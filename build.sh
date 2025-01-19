9d1f8469b0afb2fb2577208119032ebed9a7fb56
Ferico5 <huangferico@gmail.com>
2025-01-19 11:43:17 +0700

    ajarin deploy ngak, aiya

--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
-set -o errexit
+#!/bin/bash
+set -o errexit  # Exit on error
 
+# Install dependencies
 pip install -r requirements.txt
 
+# Navigate to Django project directory
 cd pos
 
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Apply database migrations
 python manage.py migrate
-if [[ $CREATE_SUPERUSER ]];
-then
+
+# Create superuser if environment variable is set
+if [[ $CREATE_SUPERUSER == "1" ]]; then
     python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
 fi
